73f3fec46710a3d3866b9a752e75ab0128a8643e
BajanVlogs <kjolly544@gmail.com>
2024-03-02 18:40:22 -0500

    Update LevelEvent.php

--- src/SignShop/EventListener/LevelEvent.php
+++ src/SignShop/EventListener/LevelEvent.php
@@ -1,26 +1,11 @@
 <?php
-/**
- * SignShop Copyright (C) 2015 xionbig
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * @author xionbig
- * @name SignShop
- * @main SignShop\SignShop
- * @link http://xionbig.netsons.org/plugins/SignShop 
- * @link http://forums.pocketmine.net/plugins/signshop.668/
- * @description Buy and Sell the items using Signs with virtual-money.
- * @version 1.1.2
- * @api 1.11.0
- */
+
 namespace SignShop\EventListener;
 
 use SignShop\SignShop;
 use pocketmine\event\Listener;
-use pocketmine\event\level\LevelLoadEvent;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 
 class LevelEvent implements Listener{
     private $SignShop;
@@ -29,7 +14,12 @@ class LevelEvent implements Listener{
         $this->SignShop = $SignShop;        
     }
     
-    public function levelLoad(LevelLoadEvent $event){
-        $this->SignShop->getSignManager()->reload($event->getLevel());
-    }    
+    public function onDataPacketReceive(DataPacketReceiveEvent $event){
+        $packet = $event->getPacket();
+        if($packet instanceof LevelSoundEventPacket && $packet->sound === LevelSoundEventPacket::SOUND_PLACE){
+            $player = $event->getPlayer();
+            $world = $player->getWorld();
+            $this->SignShop->getSignManager()->reload($world);
+        }
+    }
 }
